2dc36181e576f56ff5e49998db76b7a326deae3d
Prasan Jain <prasanjain914@gmail.com>
2022-11-29 11:10:40 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [mode, setMode] = useState('light');
+const [alert, setAlert] = useState(null)
+
+const showAlert = (message, type)=>{
+  setAlert({
+    msg:message,
+    type:type
+  })
+  setTimeout(() => {
+    setAlert(null);
+  }, 1500);
+}
+const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode ('dark') ;
+      document.body.style.backgroundColor ='#042743';
+      showAlert("Dark mode has been enabled","success");
+      document.title = 'TextUtils - Dark Mode';
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
+      document.title = 'TextUtils - Light Mode';
+    }
+}  
+
+return (
+    <>
+     {/* <Router> */}
+    <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+     <div className='container my-3'>
+         {/* <Routes>
+
+          <Route path="/" element= { */}
+          
+    <TextForm  showAlert={showAlert} heading ="Enter the text below to analyze here " 
+      mode={mode}/>
+          
+          {/* //  }/> */}
+
+          {/* <Route path="/about" element={<About />}/> */}
+            
+          
+     {/* </Routes> */}
+     </div>
+     {/* </Router> */}
+     
+   </>
+   );
 }
 
 export default App;
