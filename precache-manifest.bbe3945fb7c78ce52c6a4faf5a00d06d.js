a605ccc9aa32a0fa8f1bb7e489615047e07b1725
impactPRASANjain <jainprasan72@gmail.com>
2022-12-01 13:34:04 +0530

    Updates

---
+++ precache-manifest.bbe3945fb7c78ce52c6a4faf5a00d06d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7725c8572c846d9af462",
+    "url": "/my-app/static/css/main.93b43cac.chunk.css"
+  },
+  {
+    "revision": "7725c8572c846d9af462",
+    "url": "/my-app/static/js/main.7725c857.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/my-app/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/my-app/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "99b7f12dffb4a4f3de0c",
+    "url": "/my-app/static/js/runtime~main.99b7f12d.js"
+  },
+  {
+    "revision": "8a603976a54ba80f4e032b71cbef56ea",
+    "url": "/my-app/index.html"
+  }
+];
